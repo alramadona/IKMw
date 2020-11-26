c6861a0c76ef00a4378639366cfe218e58de172c
Aditya L. Ramadona <alramadona@outlook.com>
2020-11-26 07:18:05 +0700

    add "animating_covid19_mobility"

--- datRutin01b.R
+++ datRutin01b.R
@@ -110,3 +110,44 @@ biblioshiny()
 
 
 # Ch-2 --------------------------------------------------------------------
+# https://cran.r-project.org/web/packages/covid19mobility/vignettes/animating_covid19_mobility.html
+library(covid19mobility) #to get data on mobility from Apple/Google
+library(dplyr) # for joining, piping data frames
+library(lubridate) # for dealing with our date data
+library(sf) # shapefile manipulation
+# install.packages("devtools")
+# devtools::install_github("UrbanInstitute/urbnmapr")
+library(urbnmapr) #to get county and state data in a shapefile
+library(gganimate) # to animate plots
+
+head(refresh_covid19mobility_apple_subregion())
+
+apple_us <- refresh_covid19mobility_apple_subregion() %>%
+  filter(country=="United States") %>% # we are only going to map the US
+  group_by(location,date) %>% #we are going to group by location (which is the state) and date
+  summarize_if(is.numeric, mean) %>% #we are going to take the mean mobility score across counties by date and state
+  mutate(weeks=round_date(date, "weeks")) %>% #we are going to take only the first date of each week
+  group_by(weeks) %>% # we are going to group by each week
+  filter(date==min(weeks)) #again we want to take only the first date of each week
+
+states_sf <- urbnmapr::get_urbn_map("states", sf=TRUE) %>%
+  st_as_sf() # coerce this to a shapefile.
+
+total_apple_us  <- left_join(apple_us,states_sf, by=c("location"="state_name")) %>%
+  st_as_sf() # this function makes sure the shape files are readable.
+
+total_apple_us <- as.data.frame(total_apple_us) %>%
+  st_as_sf()
+
+ggplot() +
+  geom_sf(data=states_sf)
+
+ggplot() + #call ggplot
+  geom_sf(data=states_sf) + # map the shape files to the map of the USA
+  geom_sf(data= total_apple_us, aes(fill=value)) + # fill with our data frame and value, which is mobility score
+  labs(fill="% change in mobility", 
+       title="Date: {current_frame}", 
+       subtitle="Percent Change in Movement, Source: Apple Mobility Data",
+       fill = "% change in mobility") + #add titles
+  scale_fill_viridis_c() +
+  transition_manual(date) #animate by day - this is a gganimate function!
